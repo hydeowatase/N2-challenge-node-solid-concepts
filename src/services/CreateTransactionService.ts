7da0700c2d38c99d053a11698fa9d69b3a9f012d
hydeowatase <hydeowatase@gmail.com>
2020-11-09 23:39:14 -0300

    N2-Challenge: Back-end aplying SOLID and TS done.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: RequestDTO): Transaction {
+    if (!['income', 'outcome'].includes(type)) {
+      throw new Error('Tipo da transação inválida!');
+    }
+
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
